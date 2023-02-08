4550cf988fb560ae568ccb9bcf99a674cda134c6
archieew <123342258+archieew@users.noreply.github.com>
2023-02-08 16:06:47 +0800

    2 files added

---
+++ prelims/Prelim-exam/app.js
@@ -0,0 +1,3 @@
+var message = require('./greet.js');
+var name = 'Archie';
+console.log(message.greet1(name) + message.greet2(name) + message.greet3(name));
---
+++ prelims/Prelim-exam/greet.js
@@ -0,0 +1,13 @@
+function greet1(name) {
+  return 'Hello,' + name + '!,';
+}
+
+function greet2(name) {
+  return ' Congratulations,' + name + '!,';
+}
+
+function greet3(name) {
+  return ' Goodbye, ' + name + '!,';
+}
+
+module.exports = { greet1, greet2, greet3 };
