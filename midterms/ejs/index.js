c5d8b94be5da3428f49a21174e6418817938a595
archieew <123342258+archieew@users.noreply.github.com>
2023-03-13 12:45:31 +0800

    3 files added

---
+++ midterms/ejs/index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const app = express();
+
+// serve static files 
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'My EJS App' });
+});
+
+app.listen (3000, () => {
+  console.log('Server started on port 3000');
+});
