b6de73128c7018de379526d8b3180088e2dba409
archieew <123342258+archieew@users.noreply.github.com>
2023-03-22 16:32:24 +0800

    1 file modified

--- midterms/midterm-exam/index.js
+++ midterms/midterm-exam/index.js
@@ -1,39 +1,45 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
 
-const dishes= [{
-  type: 'Sisig',
-  province: 'Pampanga',
-  price: '220',
-},
-{
-  type: 'Salpicao',
-  province: 'Quezon',
-  price: '180',
-},
-{
-  type: 'Bagnet', 
-  province: 'Ilocos',
-  price: '370',
-}
+const dishes = [
+  {
+    type: 'Sisig',
+    province: 'Pampanga',
+    price: '220',
+  },
+  {
+    type: 'Salpicao',
+    province: 'Quezon',
+    price: '180',
+  },
+  {
+    type: 'Bagnet',
+    province: 'Ilocos',
+    price: '370',
+  },
 ];
 
-app.get('/', (req, res) => { 
-  res.send('My New App!'); 
+app.get('/', (req, res) => {
+  res.send('My New App!');
 });
 
 app.get('/api/dishes', (req, res) => {
   res.send(dishes);
 });
 
-app.get('/api/dishes/:type', (req, res) => {
-  const dishes = dishes.find(h => h.type === parseInt(req.params.id));
-  if (!dishes) return res.status(404).send('Record not found');
-  res.send(dishes);
+app.get('/api/dishes/:search', (req, res) => {
+  const search = req.params.search;
+  const dish = dishes.find(
+    (h) =>
+      h.type.toLowerCase().includes(search.toLowerCase()) ||
+      h.province.toLowerCase().includes(search.toLocaleUpperCase()) ||
+      h.price === parseInt(search)
+  );
+
+  if (!dish) return res.status(404).send('Record not found');
+  res.send(dish);
 });
 
-app.listen (3000, () => {
+app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
-
-
